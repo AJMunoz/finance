0e324ff59bc6be0a674d8dd1806dfda645041591
AJMunoz <ajspy@hotmail.com>
2014-07-20 10:18:40 +0200

    Updated finance_array.java to create random values to the budget array

--- finance_array.java
+++ finance_array.java
@@ -2,31 +2,41 @@ import java.util.*;
 public class finance_array
 {
    public static void main(String[] args) {
-       int numberOfValues = 9;
-       int[] a = {12,58,65,74,105,47,96,87,104};
+       Random rd = new Random();
        int i = 0;
        int g = 0;
-       while (i < a.length) {
+       int[] a = new int[10];
+       //Creates Random FL Budget Values
+       for (i = 0; i < 10; i++) {
+           a[i] = rd.nextInt((150 - 75) + 1) + 75;
+        }
+        i = 0;
+        //Creates the size for the largeValues array
+       while(i < a.length) {
            if(a[i] > 100) {
                g++;
             }
             i++;
        }
-       i = 0;
        int[] largeValues = new int[g];
-       while (i < largeValues.length) {
+       i = 0;
+       int k = 0;
+       //Assigns the values from a that are > 100 to largeValues
+       while(i < a.length) {
            if(a[i] > 100) {
-               largeValues[i] = a[i];
+               largeValues[k] = a[i];
+               k++;
             }
             i++;
-       }
+        }
        printArray(a);
        System.out.println();
        System.out.println();
-        
        printArray(largeValues);
        System.out.println();
        System.out.println();
+       
+       
     }
     public static void printArray(int[] a) {
         int i = 0;
